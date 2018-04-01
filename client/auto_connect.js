dd014c035a69f744281b0a1a4eb73c274e421234
KoenLav <k.lavrijssen@gmail.com>
2018-04-02 00:11:56 +0200

    Added default domain

--- client/auto_connect.js
+++ client/auto_connect.js
@@ -2,7 +2,9 @@ import { Meteor } from 'meteor/meteor';
 import LoginState from './login_state';
 
 Meteor.startup(function () {
-  const config = Meteor.settings && Meteor.settings.public && Meteor.settings.public.loginState ? Meteor.settings.public.loginState : {};
+  const config = Meteor.settings && Meteor.settings.public && Meteor.settings.public.loginState ? Meteor.settings.public.loginState : {
+    domain: '.mrwinston.nl',
+  };
 
   LoginState.init(config);
 });
