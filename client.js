02a97da72b0be43c77c0b6d8236cba657086e263
KoenLav <k.lavrijssen@gmail.com>
2018-07-17 14:34:39 +0200

    master | Refactor

--- client.js
+++ client.js
@@ -1,10 +1,12 @@
 import { checkNpmVersions } from 'meteor/tmeasday:check-npm-versions';
 import LoginState from './client/login_state';
 import './client/auto_connect';
-import './client/auto_login';
 
-checkNpmVersions({
-  'js-cookie': '^2.2.0',
-}, 'koenlav:login-state');
+checkNpmVersions(
+  {
+    'js-cookie': '^2.2.0',
+  },
+  'koenlav:login-state'
+);
 
 export default LoginState;
--- client/auto_connect.js
+++ client/auto_connect.js
@@ -1,8 +1,23 @@
 import { Meteor } from 'meteor/meteor';
 import LoginState from './login_state';
 
-Meteor.startup(function () {
-  const config = Meteor.settings && Meteor.settings.public && Meteor.settings.public.loginState ? Meteor.settings.public.loginState : {};
+Meteor.startup(function() {
+  const config =
+    Meteor.settings && Meteor.settings.public && Meteor.settings.public.loginState
+      ? Meteor.settings.public.loginState
+      : {
+          domain: '.mrwinston.nl',
+        };
 
   LoginState.init(config);
+
+  if (!Meteor.userId()) {
+    const loginState = LoginState.get();
+
+    if (loginState && loginState.loginToken && loginState.userId && loginState.loginTokenExpires) {
+      window.localStorage['Meteor.loginToken'] = loginState.loginToken;
+      window.localStorage['Meteor.userId'] = loginState.userId;
+      window.localStorage['Meteor.loginTokenExpires'] = loginState.loginTokenExpires;
+    }
+  }
 });
